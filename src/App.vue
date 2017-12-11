547f1132660dae4be3b033886ae2b8cc611d0918
Nick <zhongcheng509@gmail.com>
2017-12-11 20:11:34 +0800

    1.页面主体

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class=page>
+    <header>
+      <Topbar/>
+    </header>
+    <main>
+      <ResumeEditor/>
+      <ResumePreview/>
+    </main>
+  </div>
+</template>
+
+<script>
+  import 'normalize.css/normalize.css'
+  import './assets/reset.css'
+  import Topbar from './components/Topbar'
+  import ResumeEditor from './components/ResumeEditor'
+  import ResumePreview from './components/ResumePreview'
+
+  export default {
+    name: 'app',
+    data: function () {
+      return {
+        text: '你好'
+      }
+    },
+    components: {
+      Topbar,
+      ResumeEditor,
+      ResumePreview
+    }
+  }
+</script>
+
+<style lang="less" type="text/less">
+  .page {
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    background: #EAEBEC;
+    & > main {
+      flex-grow: 1;
+    }
+    & > main {
+      min-width: 1024px;
+      max-width: 1440px;
+      display: flex;
+      margin-top:16px;
+      margin-bottom: 16px;
+      justify-content: space-between;
+      width:100%; // 不加的话main只有1024px
+      align-self: center;
+    }
+  }
+
+  #resumeEditor {
+    width: 35%;
+    background: #444;
+  }
+
+  #resumePreview {
+    flex-grow: 1;
+    margin-left: 16px;
+    background: #777;
+  }
+
+</style>
---
+++ src/components/Topbar.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="topbar">
+    <div class="wrapper">
+      <div class="logo">Resumer</div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Topbar'
+  }
+</script>
+
+<style type="text/less" lang="less">
+  #topbar {
+    background: #fff;
+    box-shadow: 0 1px 3px 0 rgba(0,0,0,0.25);
+    height: 64px;
+    &>.wrapper{
+      min-width:1024px;
+      max-width: 1440px;
+      margin:0 auto;
+      height:64px;
+    }
+    &>.wrapper{
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 0 16px;
+    }
+    &>.logo{
+      font-size: 24px;
+      color:#000;
+    }
+  }
+</style>
